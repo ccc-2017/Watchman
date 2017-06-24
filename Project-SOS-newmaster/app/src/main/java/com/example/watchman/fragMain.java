cb5273a49cc9ed1493ab3149d9994451226f3d68
gbarcelo <gbarcelo@users.noreply.github.com>
2017-06-23 14:14:38 -1000

    Add files via upload

---
+++ Project-SOS-newmaster/app/src/main/java/com/example/watchman/fragMain.java
@@ -0,0 +1,342 @@
+package com.example.watchman;
+
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+//import android.app.Fragment;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.v4.app.ActivityCompat;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.app.Fragment;
+import android.support.v4.app.ActivityCompat;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.R.attr.delay;
+import static com.example.watchman.R.id.buttonMain;
+import static com.example.watchman.R.id.textView_Saved_Name1;
+import static com.example.watchman.R.id.textView_Saved_Num1;
+
+public class fragMain extends android.support.v4.app.Fragment implements LocationListener {
+
+
+
+    final int MAX_NUMBER__SMS_SENT = 2;
+
+    TextView textView;
+    EditText editTextPhone;
+    EditText editTextMessage;
+    Button button;
+
+    private String phonNum, msg;  // SMS, holds data
+    SmsManager smsMgrTwo = null;  // SMS, used to pass data
+    int count = 0;                // SMS, keeps track of message sent button and number putton pused.
+
+    LocationManager locationManager;  // GPS
+    String mapProvider;               // GPS
+
+    Double longCoor = 0.0;            // GPS  -long  use to pass
+    Double lattCoor = 0.0;            // Gps   Lat use to pass
+
+    String dataOfCoordNameConvertor;  // data of coordinate and name to send via SMS
+
+
+
+
+    public fragMain() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        chckPermission();
+
+        myLocationSOS();
+
+
+
+//        MessageBox(((MainActivity) getActivity()).ListAllCNums());    //debugging MsgBox
+        //
+
+
+
+// ======== WORKING
+
+
+        View rootView = inflater.inflate(R.layout.fragment_frag_main, container, false);
+
+        final View rootViewSettings = inflater.inflate(R.layout.fragment_frag_settings, container, false);
+
+        //============================ DONT DELETE ============================
+        // this button is the medical button
+        button = (Button) rootView.findViewById(R.id.buttonMain);    // button
+
+
+
+        // sndBtn Save button
+        // dsblButton  // need save button
+
+//        final Button disable = (Button) findViewById(R.id.dsblButton);       // dslbButton
+
+
+//        // Reset when SOS after it has been disabled.
+//        disable.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                count = 0;
+//            }
+//        });
+
+
+
+        button.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                // count how many text has been sent and after 2 sent. After 2 message sent, no
+                // longer need button.
+
+
+                if (count < MAX_NUMBER__SMS_SENT) {
+                    if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                        // increaseSize();
+
+                        //postCenterToast("***** HOLDING BUTTON ****** .");
+
+                    } else {//start else = = = = = = = = = = = = !!!!!!!
+
+                        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                switch (which){
+                                    case DialogInterface.BUTTON_POSITIVE:
+                                        //Yes button clicked, do nothing
+
+                                        break;
+
+                                    case DialogInterface.BUTTON_NEGATIVE:
+                                        //No button clicked
+
+                                        try {
+                                            //*****    NOTE:  THIS IS TO SEND SMS, future tech    **************
+
+                                            //  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.DONUT) {
+
+                                            smsMgrTwo = SmsManager.getDefault();
+
+
+                                            //   }
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.DONUT) {
+
+                                                // http://maps.google.com/maps?q=24.197611,120.780512 show PIC DROP
+
+                                                // adding all coordinates and message to be sent as dataOfCoordNameConvertor
+
+                                                String testMsg = "Help! Please check in on me! Here's my last known location:";
+
+                                                dataOfCoordNameConvertor =  testMsg + "  " + "http://www.google.com/maps?q="+dataOfCoordNameConvertor;
+
+                                                final String phonNum0 = ((MainActivity)getActivity()).getPhonNum(0);
+                                                final String phonNum1 = ((MainActivity)getActivity()).getPhonNum(1);
+                                                final String phonNum2 = ((MainActivity)getActivity()).getPhonNum(2);
+
+
+                                                String number = "5554";
+                                                if (phonNum0.length() > 3)
+                                                    smsMgrTwo.sendTextMessage(phonNum0, null, dataOfCoordNameConvertor, null, null);
+
+
+                                                //count++;  // count how many sms sent. . MAX 1
+
+
+                                                Handler handler = new Handler();
+                                                handler.postDelayed(new Runnable() {
+                                                    @Override
+                                                    public void run() {
+                                                        //Do something after after 1000 seconds
+                                                        if (phonNum1.length() > 3)
+                                                            smsMgrTwo.sendTextMessage(phonNum1, null, dataOfCoordNameConvertor, null, null);
+                                                    }
+                                                }, 1000);  // 1 seconds
+
+                                                Handler handler2 = new Handler();
+                                                handler2.postDelayed(new Runnable() {
+                                                    @Override
+                                                    public void run(){
+                                                        //Do something after after 1000 seconds
+                                                        if (phonNum2.length() > 3)
+                                                            smsMgrTwo.sendTextMessage(phonNum2, null, dataOfCoordNameConvertor, null, null);
+                                                    }
+                                                }, 1000);  // 1 seconds
+
+                                            }
+                                        } catch (Exception e) {
+                                            e.printStackTrace();
+                                        }
+                                        break;
+                                }
+                            }
+                        };
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                        builder.setMessage("Are you okay?").setPositiveButton("Yes", dialogClickListener)
+                                .setNegativeButton("No", dialogClickListener).show();
+
+                    } //============================================= END ELSE ===!!!!!
+
+                }// end 1st if statement ---> counting message sent
+
+                return false;
+            }
+        });
+
+        return rootView;
+    }
+
+    public void MessageBox(String message)
+    {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+    }
+
+
+    // GPS / SMS enables. .
+    public void chckPermission() {
+
+        //check permission if it is off, then it will prompt user to enable permission.
+        // Checks Location and SMS permission at the same time.
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) +
+                ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+
+            // Allow access to GPS Location, location = 0b111,
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.SEND_SMS}, 123);
+
+//            Log.d("This App", "Permission is not granted, requesting");  // SMS = 123.  123 for this High Tech
+//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, 123);
+//            //  button.setEnabled(false);
+        }
+        else {
+            Log.d("This App", "Permission is granted");
+            // postCenterToast("Permission to send SMS");.
+        }
+    }
+
+
+
+
+//=============== GPS ============================
+
+    //  1 of 2:implement GPS
+    public void myLocationSOS() {
+
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+
+        mapProvider = locationManager.getBestProvider(criteria, false);
+
+        if (mapProvider != null && !mapProvider.equals("")) {
+            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            Location location = locationManager.getLastKnownLocation(mapProvider);
+            locationManager.requestLocationUpdates(mapProvider, 15000, 1, this);
+
+            // convert decimal to string format
+            dataOfCoordNameConvertor = locationDecToString(location);
+
+            //creating location in string to pass
+            longCoor = location.getLongitude();
+            lattCoor = location.getLatitude();
+
+            if (location != null)
+                onLocationChanged(location);
+            else
+               Toast.makeText(getActivity(), "Location not Found.", Toast.LENGTH_SHORT).show();
+        }
+
+    } // end of implement GPS
+
+
+
+    // 2 of 2 implement GPS
+    @Override
+    public void onLocationChanged(Location location) {
+
+
+//        TextView longitude = (TextView) findViewById(R.id.longView);
+//        TextView latitude = (TextView) findViewById(R.id.latView);
+
+//        longitude.setText("Current Longitude:\n" + location.getLongitude());
+//        latitude.setText("Current Latitude:" + location.getLatitude());
+
+        //  String sfd = longitude.toString();  // this ts to convert. .
+
+
+
+        // send location to MSN
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+
+
+    // Converts to GPS decimal to string format and returns a string
+    public static String locationDecToString(final Location location) {
+        return Location.convert(location.getLatitude(), Location.FORMAT_DEGREES) +
+                "," + Location.convert(location.getLongitude(), Location.FORMAT_DEGREES);
+    }
+
+
+
+
+
+
+
+
+
+
+
+}// end
+
+
+
